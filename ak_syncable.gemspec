0e59beb0c53ee0402176bc2b28e7354a9c0bd092
Aaron Lipman <alipman88@gmail.com>
2014-03-21 15:21:29 -0400

    initial commit

---
+++ ak_syncable.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'sk_syncable'
+  s.version     = '0.0.0'
+  s.date        = '2014-03-21'
+  s.summary     = "Easily sync any Rails model to ActionKit"
+  s.description = "Built by Democracy for America"
+  s.authors     = ["Aaron Lipman"]
+  s.email       = 'alipman@democracyforamerica.com'
+  s.files       = ["lib/ak_syncable.rb"]
+  s.homepage    =
+    'https://github.com/Democracy-for-America/'
+  s.add_runtime_dependency 'httparty'
+  s.license       = 'MIT'
+end
---
+++ lib/ak_syncable.rb
@@ -0,0 +1,64 @@
+module AkSyncable
+
+  @@actionkit_user_fields = [:address1,:address2,:city,:country,:email,:first_name,:last_name,:middle_name,:name,:phone,:postal,:prefix,:region,:state,:suffix,:zip]
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def syncs_to(page)
+      self.class_variable_set :@@actionkit_page, page
+    end
+  end
+
+  def sync_to_actionkit
+
+    actionkit_attributes = self.class.class_variable_get :@@synced_attributes
+    actionkit_page = self.class.class_variable_get :@@actionkit_page
+
+    body = Hash.new
+
+    case actionkit_page
+    when String
+      body[:page] = actionkit_page
+    when Symbol
+      body[:page] = self.send(actionkit_page)
+    when Proc
+      body[:page] = self.instance_exec(&actionkit_page)
+    end
+
+    actionkit_attributes.each do |attribute|
+      if @@actionkit_user_fields.include? attribute.to_sym
+        body[attribute] = self[attribute]
+      else
+        body['action_' + attribute.to_s] = self[attribute]
+      end
+    end
+
+    result = HTTParty.post(ENV['ACTIONKIT_PATH'] + 'action/', basic_auth: {username: ENV['ACTIONKIT_USERNAME'], password: ENV['ACTIONKIT_PASSWORD']}, body: body)
+    
+    # Create the parent page if it does not exist.
+    if result.response.class == Net::HTTPBadRequest
+      HTTParty.post(ENV['ACTIONKIT_PATH'] + 'petitionpage/', basic_auth: {username: ENV['ACTIONKIT_USERNAME'], password: ENV['ACTIONKIT_PASSWORD']}, body: {name: body[:page], title: body[:page]})
+      second_attempt = HTTParty.post(ENV['ACTIONKIT_PATH'] + 'action/', basic_auth: {username: ENV['ACTIONKIT_USERNAME'], password: ENV['ACTIONKIT_PASSWORD']}, body: body)
+      raise 'Failed to sync with ActionKit' if JSON.parse(second_attempt.body)['id'].nil?
+    else
+      raise 'Failed to sync with ActionKit' if JSON.parse(result.body)['id'].nil?
+    end
+
+    actionkit_id = JSON.parse(result.body)['id'] || JSON.parse(second_attempt.body)['id']
+
+    if self.has_attribute? :actionkit_id
+      self.update_column :actionkit_id, actionkit_id
+    end
+
+    return actionkit_id
+
+  end
+
+  def sync_to_actionkit_with_delay
+    self.delay.sync_to_actionkit
+  end
+
+end
